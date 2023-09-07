72712f405e40fc342cc293a86e202cf950df45b5
ChocoFairy <319592692@qq.com>
2023-09-07 20:14:55 +0800

    login && logout

--- src/main/java/com/nowcoder/community/config/AlphaConfig.java
+++ src/main/java/com/nowcoder/community/config/AlphaConfig.java
@@ -8,26 +8,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 import java.text.SimpleDateFormat;
 
 @Configuration
-public class AlphaConfig extends WebMvcConfigurationSupport {
-    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-            "classpath:/META-INF/resources/", "classpath:/resources/",
-            "classpath:/static/", "classpath:/public/" };
-
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        if (!registry.hasMappingForPattern("/webjars/**")) {
-            registry.addResourceHandler("/webjars/**").addResourceLocations(
-                    "classpath:/META-INF/resources/webjars/");
-        }
-        if (!registry.hasMappingForPattern("/**")) {
-            registry.addResourceHandler("/**").addResourceLocations(
-                    CLASSPATH_RESOURCE_LOCATIONS);
-        }
-
-    }
-    @Bean
-    public SimpleDateFormat simpleDateFormat() {
-        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    }
+public class AlphaConfig{
+        //extends WebMvcConfigurationSupport {
+//    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+//            "classpath:/META-INF/resources/", "classpath:/resources/",
+//            "classpath:/static/", "classpath:/public/" };
+//
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        if (!registry.hasMappingForPattern("/webjars/**")) {
+//            registry.addResourceHandler("/webjars/**").addResourceLocations(
+//                    "classpath:/META-INF/resources/webjars/");
+//        }
+//        if (!registry.hasMappingForPattern("/**")) {
+//            registry.addResourceHandler("/**").addResourceLocations(
+//                    CLASSPATH_RESOURCE_LOCATIONS);
+//        }
+//
+//    }
+//    @Bean
+//    public SimpleDateFormat simpleDateFormat() {
+//        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//    }
 }
