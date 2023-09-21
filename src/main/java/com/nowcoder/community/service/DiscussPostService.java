c26b3a93abf1ed973911f3adfb763ead9d656452
ChocoFairy <319592692@qq.com>
2023-09-21 18:42:16 +0800

    see discuss post

--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -34,4 +34,7 @@ public class DiscussPostService {
 
         return discussPostMapper.insertDiscussPost(post);
     }
+    public DiscussPost findDiscussPostById(int id){
+        return discussPostMapper.selectDiscussPostById(id);
+    }
 }
