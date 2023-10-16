95c3966a598bbbe9d1fe1dadc72ee24bfb1fa7fc
ChocoFairy <319592692@qq.com>
2023-10-16 21:43:49 +0800

    Exception

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -176,4 +176,7 @@ public class UserService implements CommunityConstant {
         userMapper.updatePassword(userId, newPassword);
         return map;
     }
+    public User findUserByName(String username){
+        return userMapper.selectByName(username);
+    }
 }
