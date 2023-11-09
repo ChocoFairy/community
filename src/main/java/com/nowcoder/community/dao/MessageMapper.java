91d89f9d5031d298fae1d4f5f1f84da1c35661fd
ChocoBerry <319592692@qq.com>
2023-11-09 21:05:38 +0800

    show system notice

--- src/main/java/com/nowcoder/community/dao/MessageMapper.java
+++ src/main/java/com/nowcoder/community/dao/MessageMapper.java
@@ -21,5 +21,10 @@ public interface MessageMapper {
     int insertMessage(Message message);
     //修改消息状态
     int updateStatus(List<Integer> ids,int status);
-
+    //查询某个主题下的最新通知
+    Message selectLatestNotice(int userId,String topic);
+    //查询某个主题包含的通知数量
+    int selectNoticeCount(int userId,String topic);
+    //查询未读通知数量
+    int selectNoticeUnreadCount(int userId,String topic);
 }
